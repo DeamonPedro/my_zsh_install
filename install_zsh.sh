b846fb5411f1b4310ca4c53f8f2e959d37f0d102
DeamonPedro <pedro.lucas.root@gmail.com>
2019-07-13 23:40:42 -0300

    v1.2

--- install_zsh.sh
+++ install_zsh.sh
@@ -7,7 +7,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git  ~/.oh-my-zsh
 git clone https://github.com/romkatv/powerlevel10k.git  ~/.oh-my-zsh/custom/themes/powerlevel10k
 git clone https://github.com/amstrad/oh-my-matrix ~/.oh-my-zsh/custom/plugins/oh-my-matrix
 sudo mkdir "/usr/share/fonts/truetype/Hack Nerd Font"
-mv "Hack Regular Nerd Font Complete.ttf" "/usr/share/fonts/truetype/Hack Nerd Font/"
+cp "Hack Regular Nerd Font Complete.ttf" "/usr/share/fonts/truetype/Hack Nerd Font/"
 mv ~/.zshrc ~/.zshrc_old
 cp .zshrc ~/.zshrc
 cp p10k.zsh ~/.oh-my-zsh/custom/themes/powerlevel10k/internal/
